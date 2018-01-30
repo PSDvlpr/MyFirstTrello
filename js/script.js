7b1e990e18bcfcc00a61d7de0d56f3ff96573633
Piotr Strzelecki <strzelecki.piotras@gmail.com>
2018-01-30 19:19:55 +0100

    Make some kanban magic

--- js/script.js
+++ js/script.js
@@ -21,19 +21,19 @@ $(function() {
       var $column = $('<div>').addClass('column');
       var $columnTitle = $('<h2>').addClass('column-title').text(self.name);
       var $columnCardList = $('<ul>').addClass('column-card-list');
-      var $columnDelete = $('<button>').addClass('btn-delete').text('x');
-      var $columnAddCard = $('button').addClass('add-card').text('Add a card');
+      var $columnDelete = $('<button>').addClass('btn-delete btn btn-dark').text('x');
+      var $columnAddCard = $('<button>').addClass('add-card btn btn-light').text('Add a card');
 
-      $columnDelete.click(function(){
+      $columnDelete.click(function() {
         self.removeColumn();
       });
 
-      $columnAddCard.click(function(){
+      $columnAddCard.click(function() {
         self.addCard(new Card(prompt("Enter the name of the card")));
       });
 
       $column.append($columnTitle)
-              .append($columnCardList)
+              .append($columnDelete)
               .append($columnAddCard)
               .append($columnCardList);
       return $column;
@@ -54,12 +54,12 @@ $(function() {
 
     this.id = randomString();
     this.description = description;
-    this.$element = $createCard();
+    this.$element = createCard();
 
     function createCard () {
       var $card = $('<li>').addClass('card');
       var $cardDescription = $('<p>').addClass('card-description').text(self.description);
-      var $cardDelete = $('<button>').addClass('btn-delete').text('x');
+      var $cardDelete = $('<button>').addClass('btn-delete btn btn-dark').text('x');
 
       $cardDelete.click(function(){
         self.removeCard();
@@ -71,7 +71,51 @@ $(function() {
     }
   }
 
+  Card.prototype = {
+    removeCard: function() {
+      this.$element.remove();
+    }
+  };
 
+  var board = {
+    name: 'Kanban Board',
+    addColumn: function(column) {
+      this.$element.append(column.$element);
+      initSortable();
+    },
+    $element: $('#board .column-container')
+  };
 
+  function initSortable() {
+    $('.column-card-list').sortable({
+      connectWith: '.column-card-list',
+      placeholder: '.card-placeholder',
+      dropOnEmpty: true
+    }).disableSelection();
+  }
 
+  $('.create-column').click(function(){
+    var name = prompt('Enter a column name');
+    var column = new Column(name);
+      board.addColumn(column);
+  });
+
+  // CREATING COLUMNS
+var todoColumn = new Column('To do');
+var doingColumn = new Column('Doing');
+var doneColumn = new Column('Done');
+
+// ADDING COLUMNS TO THE BOARD
+board.addColumn(todoColumn);
+board.addColumn(doingColumn);
+board.addColumn(doneColumn);
+
+// CREATING CARDS
+/*var card1 = new Card('New task');
+var card2 = new Card('Create kanban boards');
+
+// ADDING CARDS TO COLUMNS
+todoColumn.addCard(card1);
+doingColumn.addCard(card2);
+*/
 });
